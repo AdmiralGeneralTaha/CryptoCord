797f4c4dbe3331fa6b12e753573343ebfd19ede6
Mirza Taha <78162863+AdmiralGeneralTaha@users.noreply.github.com>
2022-06-21 11:30:35 +0100

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,14 @@
+currencies = {
+  "btc": "bitcoin",
+  "eth": "ethereum",
+  "shiba": "shiba-inu",
+  "doge": "dogecoin",
+  "ltc": "litecoin",
+  "usdt": "tether",
+}
+status = ["£bitcoin", "$bitcoin", "£ethereum", "$ethereum", "£dogecoin", "$dogecoin",
+          "£cardano", "$cardano"]
+
+reply = ['daddy elon😍', '2DaMoon!', 'bitcoin!']
+
+clientCurrencies = ["£", "$", "€"]
